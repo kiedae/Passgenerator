1f27fbaa94469c586a1fe96c92a7e87a00a751fd
Kiedae <ernestwilliams335@gmail.com>
2023-08-05 22:23:03 -0400

    updated starter code given but ASKBCS mentor, added onto that adding variables for lowercase uppercase and special characters, added if statements so users couldnt input number higher than 128

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Recommend making variables for characters an array of strings as this is easier to manage!
+var numberCharacters = ['0','1','2','3','4','5','6','7','8','9'];
+var lowercaseCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "-", "_"]
+
+function promptUser() {
+  var passwordLength = parseInt(prompt('How many characters would you like your password length to contain?'));
+    
+  if (parseInt == isNaN) {
+    alert('Password length must be a number.');
+    return null;
+}
+
+  if (passwordLength < 8) {
+    alert('Password length needs to be at least 8 characters in length');
+    return null;
+  }
+
+  if (passwordLength > 128) {
+    alert('Password length cannot exceed 128 characters in length.');
+    return null;
+}
+ 
+
+
+  var hasNumbers = confirm('Click OK to confirm you would like your password to include numbers!');
+
+  var hasUppercase = confirm('Click OK to confirm you would like your password to include uppercase letters.');
+  
+  var hasLowercase = confirm('Click ok if you would like your password to include lowercase letters.');
+  
+  var hasSpecial = confirm('Click OK to confirm you would like to have your password include special characters.');
+  
+
+  
+  console.log(passwordLength);
+  console.log(hasNumbers);
+  console.log(hasUppercase);
+  console.log(hasLowercase);
+  console.log(hasSpecial);
+}
+
+function generatePassword () {
+  var options = promptUser();
+  console.log(options)
+}
+
 // Write password to the #password input
-
-var lowcase = "abcdefghijklmnoprstuvwxyz"
-var upcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numercl = "1234567890"
-
-
-
-
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -20,8 +60,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-function confirm() {
-  return confirm("Would you like to include lowercase letters?");
-
-}
